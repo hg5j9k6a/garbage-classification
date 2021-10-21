a0b2fcbb4227e8cb79e2bce7c2ac8a89ef9e42c2
hg5j9k6a <48769492+hg5j9k6a@users.noreply.github.com>
2021-10-21 17:58:58 +0800

    Add files via upload

---
+++ src/object_pos.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+class object_pos():
+    def __init__(self):
+        # box pos
+        self.palstic_box    =   {"x":-0.65,"y":1.2,"z":0.00,"roll":0.0,"pitch":0.0,"yaw":-1.8}
+        self.can_box        =   {"x":-0.65,"y":1.2,"z":0.40,"roll":0.0,"pitch":0.0,"yaw":-1.8}
+        self.paper_box      =   {"x":-0.40,"y":1.0,"z":0.80,"roll":0.0,"pitch":0.0,"yaw":-1.8}
+        self.glass_box      =   {"x":-0.65,"y":1.2,"z":-0.40,"roll":0.0,"pitch":0.0,"yaw":-2.4}
+        self.palstic_bag_box=   {"x":-0.40,"y":1.1,"z":-0.68,"roll":0.0,"pitch":0.0,"yaw":-0.0}
+        
+        # object_pos
+        self.beer_bottle    =   {"x":0.0,"y":0.80,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":70.0}
+        self.beer_can       =   {"x":0.0,"y":0.80,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":70.0}
+        self.milk_250ml     =   {"x":0.0,"y":0.75,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":90.0}
+        self.milk_2L        =   {"x":0.0,"y":0.85,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":50.0}
+        self.coke_bottle    =   {"x":0.0,"y":0.78,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":80.0}
+        self.coke_can       =   {"x":0.0,"y":0.78,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":80.0}
+        self.water_bottle   =   {"x":0.0,"y":0.88,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":65.0}
+        self.tissue         =   {"x":0.0,"y":0.78,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":20.0}
+        self.pain_killer    =   {"x":0.0,"y":0.75,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":90.0}
+        self.chip_bag       =   {"x":0.0,"y":0.70,"z":0.0,"roll":1.57,"pitch":0.0,"yaw":0.0,"grip":70.0}
+        self.wine_bottle    =   {"x":0.0,"y":0.85,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":75.0}
+        self.coffee_can     =   {"x":0.0,"y":0.78,"z":0.0,"roll":0.0,"pitch":0.0,"yaw":-0.8,"grip":50.0}
+        
